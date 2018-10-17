0f65f53502b539053bc3d7df6328627a39b9f3be
Matheus C. Ambrozio <mambrozio@users.noreply.github.com>
2018-10-17 14:08:18 -0300

    More comments and mpi message structure definition

--- src/problem-02/main.c
+++ src/problem-02/main.c
@@ -1,34 +1,42 @@
 /**/
 
 #include <stdio.h>
+#include <stddef.h> //ofsetof
 
 #include "mpi.h"
 
 #define EPSILON 0.0000000000000001
-//define TAGS
+#define EMPTY 0
 
-/*
-#define B_SIZE  200
+// TAGS
+#define TAG_WANT_INTERVAL  3//Calculating node wants interval from master
+#define TAG_PARTIAL_RESULT 5//Calculating node sends partial result to master
+#define TAG_NEW_INTERVAL   7//Calculating node sends new interval to master
 
-typedef struct Buffer {
-    int pieces; //current number of elements
-    int size; //total number of elements
-    int b[B_SIZE];
-} Buffer;
-*/
+typedef struct Message {
+    int a;
+    int b;
+    double res;
+} Message;
 
 void master();
 void calculate();
 
+/* Global variables */
+int rank;
+int no_processes; //need to be global so master can access to create waiting_queue
+
 int main(int argc, char** argv) {
-    int rank;
-    int no_processes;
 
     MPI_Init(&argc, &argv);
-
     MPI_Comm_size(MPI_COMM_WORLD, &no_processes);
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Create new mpi type for struct Message
+    MPI_Datatype mpi_dt_message;
+    MPI_Type_create_struct(3, {MPI_INT, MPI_INT, MPI_DOUBLE}, {1, 1, 1},
+        {offsetof(Message, a), offsetof(Message, b), offsetof(Message, res)},
+        &mpi_dt_message);
 
     if (rank == 0) { /* Master node */
         master();
@@ -43,21 +51,37 @@ int main(int argc, char** argv) {
 
 void master() {
     MPI_Status status;
+    unsigned waiting[no_processes];
+    Message msg;
 
-    //initialize buffer
+    //initialize arrays
+    for (int i = 0; i < no_processes; i++) {
+        waiting[i] = EMPTY;
+    }
 
     for(;;) {
         //receive any tag
+        MPI_Recv(&msg, mpi_dt_message, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        
         //if tag is get_interval, send next interval (protect buffer?)
-        //if tag is partial_result, sum to partial result variable and sum the intervals.
-        //if all intervals were calculated,
+        //if tag is partial_result, sum to partial result variable and sum the intervals
+        //if tag is new_interval, insert new_interval in intervals list
+        //if all intervals were calculated, send message to all nodes to terminate
     }
+
+    return;
 }
 
 void calculate() {
     MPI_Status status;
 
     for(;;) {
-
+        //check if asking for interval is needed
+        //calculate interval
+        //if split is needed, make new interval, send one to master, update current interval
+        //else, send result to master and clean current interval
     }
+
+    return;
 }
