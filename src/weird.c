7a9a76e189c705be8471972f9659986170b423d6
Renan <renan.061@gmail.com>
2018-10-23 23:33:41 -0300

    Adds the complete report and changes some minor things

--- src/weird.c
+++ src/weird.c
@@ -1,7 +1,7 @@
 #include <math.h>
 
-// (x - 1) * (x + 3)^2 * (x - 2)^3 * (x + 1)^4 [-3, 2]
-double weird2(double x) {
+// (x - 1) * (x + 3)^2 * (x - 2)^3 * (x + 1)^4
+double weird1(double x) {
     double a = x - 1;
     double b = pow(x + 3, 2);
     double c = pow(x - 2, 3);
@@ -9,6 +9,6 @@ double weird2(double x) {
     return a * b * c * d;
 }
 
-double weird1(double x) {
+double weird2(double x) {
     return exp(sin(x));
 }
